16bc3657147c13e05a7e01ecf14d23c159f83a5d
Hakan ÖZER <hakanozer02@gmail.com>
2026-09-12 10:45:22 +0300

    Days_1

---
+++ src/main/java/com/works/Bilgeadam2026IleriJavaApplication.java
@@ -0,0 +1,13 @@
+package com.works;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bilgeadam2026IleriJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bilgeadam2026IleriJavaApplication.class, args);
+    }
+
+}
